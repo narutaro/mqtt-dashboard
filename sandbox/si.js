972c1fed4b65313dedb2027d68a5e6420ec089a7
narutaro <narusan@mac.com>
2020-05-01 11:51:55 +0900

    clearn up

---
+++ sandbox/si.js
@@ -0,0 +1,6 @@
+setInterval(() => {
+    const message = Date.now().toString();
+    //client.publish('n0bisuke', message);
+    //console.log('publisher.publish:', message);
+    console.log(message)
+}, 1000);
